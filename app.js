8dbbb1cd8d324e6f6b6123716d5fc488af0fa588
chenxingyu350128 <991986182@qq.com>
2018-11-06 16:55:58 +0800

    aaa

---
+++ app.js
@@ -0,0 +1,37 @@
+//app.js
+App({
+  onLaunch: function () {
+   /*"tabBar": {
+    "selectedColor": "#00bfff",
+    "list": [
+      {
+        "pagePath": "pages/tab/index",
+        "text": "展业海报",
+        "iconPath": "/assets/index.png",
+        "selectedIconPath": "/assets/indexa.png"
+      },
+      {
+        "pagePath": "pages/tab/video",
+        "text": "展业视频",
+        "iconPath": "/assets/video.png",
+        "selectedIconPath": "/assets/videoa.png"
+      },
+      {
+        "pagePath": "pages/tab/friends",
+        "text": "朋友圈",
+        "iconPath": "/assets/firends.png",
+        "selectedIconPath": "/assets/firendsa.png"
+      },
+      {
+        "pagePath": "pages/tab/user",
+        "text": "个人中心",
+        "iconPath": "/assets/user.png",
+        "selectedIconPath": "/assets/usera.png"
+      }
+    ]
+  },*/
+  },
+  globalData: {
+    userInfo: null
+  }
+})
